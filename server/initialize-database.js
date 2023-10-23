4ad3e6f74ef86cc5ed3759e064eac609ba84ae5f
StevenNguyenUIT <143907704+StevenNguyenUIT@users.noreply.github.com>
2023-10-23 15:02:09 -0500

    update json test and localhost

--- server/initialize-database.js
+++ server/initialize-database.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const url = 'mongodb://localhost:27017';
+const url = 'mongodb://0.0.0.0:27017';
 const dbName = 'aero-tran';
 
 async function initializeDatabase() {

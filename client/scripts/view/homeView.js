6ecbc75d94d8d5ba305e74e510674afa0adb47bc
herotran0504 <herotran0504@gmail.com>
2023-10-27 18:22:53 -0500

    - handle my bookings visibility

--- client/scripts/view/homeView.js
+++ client/scripts/view/homeView.js
@@ -4,12 +4,14 @@ export class HomeView {
         $('#login').hide();
         $('#userInfo').show();
         $('#logout').show();
+        $('#bookings').show();
     }
 
     showGuestControls() {
         $('#login').show();
         $('#userInfo').hide();
         $('#logout').hide();
+        $('#bookings').hide();
     }
 
 }
